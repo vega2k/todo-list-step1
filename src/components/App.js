3ffbb94357f43ddf3c228f689307405632b245d8
vega2k <vega2k@naver.com>
2018-08-08 17:44:24 +0900

    todoinput, todoitem, todolist 컴포넌트 추가

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import PageTemplate from "./PageTemplate";
+import TodoInput from "./TodoInput";
+import TodoList from "./TodoList";
+
+class App extends Component {
+    render() {
+        return (
+                <PageTemplate>
+                    <TodoInput/>
+                    <TodoList/>
+                </PageTemplate>
+        );
+    }
+}
+export default App;
